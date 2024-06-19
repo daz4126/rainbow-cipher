cf0d3f9361c0cbbe8668c626a391d3e3169a1fd9
DAZ <daz4126@gmail.com>
2024-06-19 14:54:01 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,126 +1,98 @@
 // imports
-import { Nanny,html } from 'nanny-state';
-// Helper Functions
-// (number,number) => number
-const randomNumber = (n,m) => Math.ceil(Math.random()*(n - m)+m)
-// () => string
-function generateNumber(){
-  const multipleOfThree = randomNumber(333,34)*3
-  console.log(multipleOfThree)
-  return {number: multipleOfThree.toString()}
-}
-// (array,array) => array
-function getColours(guesses,number){
-  return guesses.map((digit,index) => number.includes(digit) ? digit.toString() === number[index] ? "green" : "yellow": "black")
-}
-// View
-// state => html string
-const View = state => html`
-<div id="app" ?data-darkMode=${state.dark}>
-<header>
-<h1>Numble</h1>
-${state.started ? html`<div class="scores"><p>Streak: ${state.streak}</p><p>Best: ${state.best}</p></div>` : ""}
-</header>
-${state.started ? 
-html`<div id="guesses">
-${state.guesses.map((guess,index) => html`<div class="row">${guess.map((number,i) => html`<div class="${state.guessCount > index && getColours(guess,state.number)[i]}"}>${number}</div>`)}</div>`)}</div>
-<p id="feedback">${state.feedback}</p>
-<div id="keyboard">
-${state.digits.map((colour,index) => html`<button onclick=${e => Update(appear(index))} class="${colour}" ?disabled=${!state.playing}>${index}</button>`)}
-  <button class="functional" onclick=${e => Update(remove)}>DELETE</button>
-  <button class="functional" onclick=${check(state.guesses[state.guessCount],state.number, state.count)}>ENTER</button>
-</div>
-<button ?disabled=${state.playing} class="${state.playing ? "hidden" : ""}" onclick=${e => Update(nextNumber)}>PLAY AGAIN</button>
-<footer>
-<button onclick=${e => Update(finish)}>FINISH</button>
-<button onclick=${e => Update(e => Update({dark: !state.dark}))}>${state.dark ? "LIGHT" : "DARK"} MODE</button>
-</footer>
+import { pickRandom } from "jog-list"
+import { words } from "./words.js"
+import surge from "@daz4126/surge"
+import JSConfetti from "js-confetti"
+
+const jsConfetti = new JSConfetti()
+
+const keys = words.filter(word => word.length === new Set([...word]).size)
+const alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+const fruit = ["🍏","🍌","🍒","🍉","🍑"]
+const veg =  ["🥦","🍆","🥔","🍄","🥕"]
+
+const encrypt = (word,key) => [...word].map(letter => {
+  const char = letter === "J" ? "I" : letter
+  const cipher = key + [...alphabet].filter(x => !key.includes(x)).join``
+  const n = cipher.indexOf(char)
+  return fruit[Math.floor(n/5)] + veg[n%5] + "  "
+ }).join``
+
+const table = `
+  <table>
+    <thead>
+      <td></td>
+      <td>${veg[0]}</td>
+      <td>${veg[1]}</td>
+      <td>${veg[2]}</td>
+      <td>${veg[3]}</td>
+      <td>${veg[4]}</td>
+    </thead>
+    ${fruit.map(f => `<tr><td>${f}</td><td><input data-fruit=${f} data-veg=${veg[0]} data-action="updateSolution"></td><td><input data-fruit=${f} data-veg=${veg[1]} data-action="updateSolution"></td><td><input data-fruit=${f} data-veg=${veg[2]} data-action="updateSolution"></td><td><input data-fruit=${f} data-veg=${veg[3]} data-action="updateSolution"></td><td><input data-fruit=${f} data-veg=${veg[4]} data-action="updateSolution"></td></tr>`).join``}
+  </table>
 `
-:
-html`
-<p>Guess the 3-digit number in four or less tries to get a Numble.</p>
-<p>The number is always a multiple of 3 (so the digits must add up to a multiple of 3).</p>
-<p>After each guess, the digits will change colour to show how close you were to getting a Numble.</p>
-<h3>Examples</h3>
-<div class="guesses"><div class="row"><div class="green">1</div><div>2</div><div>3</div></div></div>
-<p>1 is in the number and also in the right place.</p>
-<div class="guesses"><div class="row"><div>4</div><div class="yellow">5</div><div>6</div></div></div>
-<p>5 is in the number but in the wrong place place.</p>
-<div class="guesses"><div class="row"><div>7</div><div>8</div><div class="black">9</div></div></div>
-<p>9 is not in the number at all.</p>
-<h2>How many Numbles can you get?</h2>
-<p>When you get a Numble, keep going and get a streak of Numbles!</p>
-<button class="action" onclick=${e => Update(start)}>START</button>`}</div>
-`
-<button class="action" onclick=${e => Update(start)}>START</button>`}</div>
-`
-// initial state
-const State = {
-  ...generateNumber(),
-  started: false,
-  playing: false,
-  digits: Array(10).fill('grey'),
-  guesses: [Array(3).fill(null),Array(3).fill(null),Array(3).fill(null),Array(3).fill(null)],
-  count: 0,
-  guessCount: 0,
-  feedback: "Guess 3 digits",
-  streak: 0,
-  best: 0,
-  LocalStorageKey: "numble-local-storage",
-  dark: false,
-  View
-}
-// Event handlers
-const check = (guesses, number, count) => event => {
-  if (count < 3) Update({feedback: "too short"})
-  else {
-    const numble = guesses.join('') === number
-    const colours = getColours(guesses,number)
-  Update(provideFeedback(guesses,colours, numble))
+
+surge({
+  start: $ => e => {
+    $.instructions.hidden = true
+    $.gameOver.hidden = true
+    $.game.hidden = false
+    Array.from($.solution.childNodes).forEach(cell => cell.value = "")
+    Array.from($.table.querySelectorAll("input")).forEach(cell => cell.value = "")
+    $.table.value = ""
+    $.clues.value = ""
+    $.score.value = 10
+    $.correct.value = 0
+    $.table.append(table)
+    $._key = pickRandom(keys).toUpperCase()
+    $._word = pickRandom(words).toUpperCase()
+    $._remainingWords = words.filter(w => w !== $._word)
+    console.log($._word)
+    console.log($._key)
+    $.word.value = encrypt($._word,$._key)
+  },
+  clue: $ => e => {
+    const word = pickRandom($._remainingWords).toUpperCase()
+    $.clues.append(`<h1>${word}:</h1><h1>${encrypt(word,$._key)}</h1>`)
+    $.score.value --
+  },
+  updateGrid: $ => e => {
+    const char = $._word[Number(e.target.id.split("-")[1])]
+    const letter = char === "J" ? "I" : char
+    const cipher = $._key + [...alphabet].filter(x => !$._key.includes(x)).join``
+    const index = cipher.indexOf(letter)
+    const cell = $.table.querySelectorAll("input")[index]
+    cell.value = e.target.value
+  },
+  updateSolution: $ => e => {
+    const array = encrypt($._word,$._key).split("  ")
+    const letter = e.target.value.toUpperCase()
+    const fruit = e.target.dataset.fruit
+    const veg = e.target.dataset.veg
+    array.forEach((x,i) => {
+      if(x === fruit+veg){
+        $.solution.childNodes[i].value = (letter === "J" && $._word[i] === "I") ? "I" : (letter === "I" && $._word[i] === "J") ? "J" : letter
+      }
+    })
+  },
+    check: $ => e => {
+           const correctLetters = Array.from($.solution.childNodes).reduce((sum,node,i) => sum + (node.value.toUpperCase() === $._word[i] ? 1 : 0),0)
+     $.correct.value = correctLetters
+     if(correctLetters === 5){
+         jsConfetti.addConfetti()
+         $.game.hidden = true
+         $.gameOver.hidden = false
+         $.finalScore.value = $.score.value
+     } else {
+       $.score.value --
+     }
+     if($.score.value === 0){
+       $.game.hidden = true
+        $.gameOver.hidden = false
+       $.message.value = "Hard luck, you didn't break the code"
+     }
+  },
+  clear: $ => e => {
+    Array.from($.table.querySelectorAll("input")).forEach(cell => cell.value = "")
   }
-}
-// Transformer functions
-// state => state
-const start = state => ({
-  started: true,
-  playing: true
 })
-// array => state => state
-const appear = guess => state => ({
-  guesses: state.guesses.map((array,index) => index === state.guessCount ? array.map((digit,index) => index === state.count ? guess : digit) : array),
-  count: state.count + 1,
-})
-// state => state
-const remove = state => ({
-  guesses: state.guesses.map((array,index) => index === state.guessCount ? array.map((digit,index) => index === state.count - 1 ? null : digit) : array),
-  count: state.count - 1
-})
-// (array,array,Boolean) => state => state
-const provideFeedback = (guesses,colours,numble) => state => ({
-  digits: state.digits.map((colour,index) => guesses.includes(index) ? colours[guesses.indexOf(index)] : colour),
-  count: 0,
-  guessCount: state.guessCount + 1,
-  feedback: numble ? "NUMBLE!" : state.guessCount < 3 ? "Keep going..." : `Nope! It was ${state.number}`,
-  playing: !numble && state.guessCount < 3,
-  streak: numble ? state.streak + 1 : state.guessCount < 3 ? state.streak : 0,
-  best: numble && state.streak + 1 > state.best ? state.streak + 1 : state.best
-})
-// state => state
-const nextNumber = state => ({
-  ...State,
-  ...generateNumber(),
-  started: true,
-  playing: true,
-  streak: state.streak,
-  best: state.best,
-  dark: state.dark
-})
-// state => state
-const finish = state => ({
-  ...State,
-  ...generateNumber(),
-  best: state.best,
-  dark: state.dark
-})
-// Start a new Nanny State
-const Update = Nanny(State)
